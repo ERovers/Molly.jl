d1cdadd903b8f6c777c2f59168313519e6fb00f5
Evianne Rovers <evianne.rovers@gmail.com>
2025-10-29 15:42:07 +0000

    push, append for System

---
+++ src/interactions/.ipynb_checkpoints/lennard_jones-checkpoint.jl
@@ -0,0 +1,429 @@
+export
+    LennardJones,
+    LennardJonesSoftCore,
+    AshbaughHatch
+
+@doc raw"""
+    LennardJones(; cutoff, use_neighbors, shortcut, σ_mixing, ϵ_mixing, weight_special)
+
+The Lennard-Jones 6-12 interaction between two atoms.
+
+The potential energy is defined as
+```math
+V(r_{ij}) = 4\varepsilon_{ij} \left[\left(\frac{\sigma_{ij}}{r_{ij}}\right)^{12} - \left(\frac{\sigma_{ij}}{r_{ij}}\right)^{6}\right]
+```
+and the force on each atom by
+```math
+\begin{aligned}
+\vec{F}_i &= 24\varepsilon_{ij} \left(2\frac{\sigma_{ij}^{12}}{r_{ij}^{13}} - \frac{\sigma_{ij}^6}{r_{ij}^{7}}\right) \frac{\vec{r}_{ij}}{r_{ij}} \\
+&= \frac{24\varepsilon_{ij}}{r_{ij}^2} \left[2\left(\frac{\sigma_{ij}^{6}}{r_{ij}^{6}}\right)^2 -\left(\frac{\sigma_{ij}}{r_{ij}}\right)^{6}\right] \vec{r}_{ij}
+\end{aligned}
+```
+
+The potential energy does not include the long range dispersion correction present
+in some other implementations that approximately represents contributions from
+beyond the cutoff distance.
+"""
+@kwdef struct LennardJones{C, H, S, E, W} <: PairwiseInteraction
+    cutoff::C = NoCutoff()
+    use_neighbors::Bool = false
+    shortcut::H = lj_zero_shortcut
+    σ_mixing::S = lorentz_σ_mixing
+    ϵ_mixing::E = geometric_ϵ_mixing
+    weight_special::W = 1
+end
+
+use_neighbors(inter::LennardJones) = inter.use_neighbors
+
+function Base.zero(lj::LennardJones{C, H, S, E, W}) where {C, H, S, E, W}
+    return LennardJones(
+        lj.cutoff,
+        lj.use_neighbors,
+        lj.shortcut,
+        lj.σ_mixing,
+        lj.ϵ_mixing,
+        zero(W),
+    )
+end
+
+function Base.:+(l1::LennardJones, l2::LennardJones)
+    return LennardJones(
+        l1.cutoff,
+        l1.use_neighbors,
+        l1.shortcut,
+        l1.σ_mixing,
+        l1.ϵ_mixing,
+        l1.weight_special + l2.weight_special,
+    )
+end
+
+function inject_interaction(inter::LennardJones, params_dic)
+    key_prefix = "inter_LJ_"
+    return LennardJones(
+        inter.cutoff,
+        inter.use_neighbors,
+        inter.shortcut,
+        inter.σ_mixing,
+        inter.ϵ_mixing,
+        dict_get(params_dic, key_prefix * "weight_14", inter.weight_special),
+    )
+end
+
+function extract_parameters!(params_dic, inter::LennardJones, ff)
+    key_prefix = "inter_LJ_"
+    params_dic[key_prefix * "weight_14"] = inter.weight_special
+    return params_dic
+end
+
+@inline function force(inter::LennardJones,
+                       dr,
+                       atom_i,
+                       atom_j,
+                       force_units=u"kJ * mol^-1 * nm^-1",
+                       special=false,
+                       args...)
+    if inter.shortcut(atom_i, atom_j)
+        return ustrip.(zero(dr)) * force_units
+    end
+    σ = inter.σ_mixing(atom_i, atom_j)
+    ϵ = inter.ϵ_mixing(atom_i, atom_j)
+
+    cutoff = inter.cutoff
+    r = norm(dr)
+    σ2 = σ^2
+    params = (σ2, ϵ)
+
+    f = force_cutoff(cutoff, inter, r, params)
+    fdr = (f / r) * dr
+    if special
+        return fdr * inter.weight_special
+    else
+        return fdr
+    end
+end
+
+function pairwise_force(::LennardJones, r, (σ2, ϵ))
+    six_term = (σ2 / r^2) ^ 3
+    return (24ϵ / r) * (2 * six_term ^ 2 - six_term)
+end
+
+@inline function potential_energy(inter::LennardJones,
+                                  dr,
+                                  atom_i,
+                                  atom_j,
+                                  energy_units=u"kJ * mol^-1",
+                                  special=false,
+                                  args...)
+    if inter.shortcut(atom_i, atom_j)
+        return ustrip(zero(dr[1])) * energy_units
+    end
+    σ = inter.σ_mixing(atom_i, atom_j)
+    ϵ = inter.ϵ_mixing(atom_i, atom_j)
+
+    cutoff = inter.cutoff
+    r = norm(dr)
+    σ2 = σ^2
+    params = (σ2, ϵ)
+
+    pe = pe_cutoff(cutoff, inter, r, params)
+    if special
+        return pe * inter.weight_special
+    else
+        return pe
+    end
+end
+
+function pairwise_pe(::LennardJones, r, (σ2, ϵ))
+    six_term = (σ2 / r^2) ^ 3
+    return 4ϵ * (six_term ^ 2 - six_term)
+end
+
+@doc raw"""
+    LennardJonesSoftCore(; cutoff, α, λ, p, use_neighbors, shortcut, σ_mixing, ϵ_mixing,
+                         weight_special)
+
+The Lennard-Jones 6-12 interaction between two atoms with a soft core.
+
+The potential energy is defined as
+```math
+V(r_{ij}) = 4\varepsilon_{ij} \left[\left(\frac{\sigma_{ij}}{r_{ij}^{\text{sc}}}\right)^{12} - \left(\frac{\sigma_{ij}}{r_{ij}^{\text{sc}}}\right)^{6}\right]
+```
+and the force on each atom by
+```math
+\vec{F}_i = 24\varepsilon_{ij} \left(2\frac{\sigma_{ij}^{12}}{(r_{ij}^{\text{sc}})^{13}} - \frac{\sigma_{ij}^6}{(r_{ij}^{\text{sc}})^{7}}\right) \left(\frac{r_{ij}}{r_{ij}^{\text{sc}}}\right)^5 \frac{\vec{r}_{ij}}{r_{ij}}
+```
+where
+```math
+r_{ij}^{\text{sc}} = \left(r_{ij}^6 + \alpha \sigma_{ij}^6 \lambda^p \right)^{1/6}
+```
+If ``\alpha`` or ``\lambda`` are zero this gives the standard [`LennardJones`](@ref) potential.
+"""
+@kwdef struct LennardJonesSoftCore{C, A, L, P, H, S, E, W, R} <: PairwiseInteraction
+    cutoff::C = NoCutoff()
+    α::A = 1
+    λ::L = 0
+    p::P = 2
+    use_neighbors::Bool = false
+    shortcut::H = lj_zero_shortcut
+    σ_mixing::S = lorentz_σ_mixing
+    ϵ_mixing::E = geometric_ϵ_mixing
+    weight_special::W = 1
+    σ6_fac::R = α * λ^p
+end
+
+use_neighbors(inter::LennardJonesSoftCore) = inter.use_neighbors
+
+function Base.zero(lj::LennardJonesSoftCore{C, A, L, P, H, S, E, W, R}) where {C, A, L, P, H, S, E, W, R}
+    return LennardJonesSoftCore(
+        lj.cutoff,
+        zero(A),
+        zero(L),
+        zero(P),
+        lj.use_neighbors,
+        lj.shortcut,
+        lj.σ_mixing,
+        lj.ϵ_mixing,
+        zero(W),
+        zero(R),
+    )
+end
+
+function Base.:+(l1::LennardJonesSoftCore, l2::LennardJonesSoftCore)
+    return LennardJonesSoftCore(
+        l1.cutoff,
+        l1.α + l2.α,
+        l1.λ + l2.λ,
+        l1.p + l2.p,
+        l1.use_neighbors,
+        l1.shortcut,
+        l1.σ_mixing,
+        l1.ϵ_mixing,
+        l1.weight_special + l2.weight_special,
+        l1.σ6_fac + l2.σ6_fac,
+    )
+end
+
+@inline function force(inter::LennardJonesSoftCore,
+                       dr,
+                       atom_i,
+                       atom_j,
+                       force_units=u"kJ * mol^-1 * nm^-1",
+                       special=false,
+                       args...)
+    if inter.shortcut(atom_i, atom_j)
+        return ustrip.(zero(dr)) * force_units
+    end
+    σ = inter.σ_mixing(atom_i, atom_j)
+    ϵ = inter.ϵ_mixing(atom_i, atom_j)
+
+    cutoff = inter.cutoff
+    r = norm(dr)
+    σ2 = σ^2
+    params = (σ2, ϵ, inter.σ6_fac)
+
+    f = force_cutoff(cutoff, inter, r, params)
+    fdr = (f / r) * dr
+    if special
+        return fdr * inter.weight_special
+    else
+        return fdr
+    end
+end
+
+function pairwise_force(::LennardJonesSoftCore, r, (σ2, ϵ, σ6_fac))
+    inv_rsc6 = inv(r^6 + σ2^3 * σ6_fac) # rsc = (r^6 + α * σ2^3 * λ^p)^(1/6)
+    inv_rsc  = sqrt(cbrt(inv_rsc6))
+    six_term = σ2^3 * inv_rsc6
+    return (24ϵ * inv_rsc) * (2 * six_term^2 - six_term) * (r * inv_rsc)^5
+end
+
+@inline function potential_energy(inter::LennardJonesSoftCore,
+                                  dr,
+                                  atom_i,
+                                  atom_j,
+                                  energy_units=u"kJ * mol^-1",
+                                  special=false,
+                                  args...)
+    if inter.shortcut(atom_i, atom_j)
+        return ustrip(zero(dr[1])) * energy_units
+    end
+    σ = inter.σ_mixing(atom_i, atom_j)
+    ϵ = inter.ϵ_mixing(atom_i, atom_j)
+
+    cutoff = inter.cutoff
+    r = norm(dr)
+    σ2 = σ^2
+    params = (σ2, ϵ, inter.σ6_fac)
+
+    pe = pe_cutoff(cutoff, inter, r, params)
+    if special
+        return pe * inter.weight_special
+    else
+        return pe
+    end
+end
+
+function pairwise_pe(::LennardJonesSoftCore, r, (σ2, ϵ, σ6_fac))
+    six_term = σ2^3 * inv(r^6 + σ2^3 * σ6_fac)
+    return 4ϵ * (six_term ^ 2 - six_term)
+end
+
+@doc raw"""
+    AshbaughHatch(; cutoff, use_neighbors, shortcut, ϵ_mixing, σ_mixing,
+                  λ_mixing, weight_special)
+
+The Ashbaugh-Hatch potential ($V_{\text{AH}}$) is a modified Lennard-Jones ($V_{\text{LJ}}$)
+6-12 interaction between two atoms.
+
+The potential energy is defined as
+```math
+V_{\text{LJ}}(r_{ij}) = 4\varepsilon_{ij} \left[\left(\frac{\sigma_{ij}}{r_{ij}}\right)^{12} - \left(\frac{\sigma_{ij}}{r_{ij}}\right)^{6}\right] \\
+```
+```math
+V_{\text{AH}}(r_{ij}) =
+    \begin{cases}
+      V_{\text{LJ}}(r_{ij}) +\varepsilon_{ij}(1-λ_{ij}) &,  r_{ij}\leq  2^{1/6}σ  \\
+       λ_{ij}V_{\text{LJ}}(r_{ij})  &,  2^{1/6}σ \leq r_{ij}
+    \end{cases}
+```
+and the force on each atom by
+```math
+\vec{F}_{\text{AH}} =
+    \begin{cases}
+      F_{\text{LJ}}(r_{ij})  &,  r_{ij} \leq  2^{1/6}σ  \\
+       λ_{ij}F_{\text{LJ}}(r_{ij})  &,  2^{1/6}σ \leq r_{ij}
+    \end{cases}
+```
+where
+```math
+\begin{aligned}
+\vec{F}_{\text{LJ}}\
+&= \frac{24\varepsilon_{ij}}{r_{ij}^2} \left[2\left(\frac{\sigma_{ij}^{6}}{r_{ij}^{6}}\right)^2 -\left(\frac{\sigma_{ij}}{r_{ij}}\right)^{6}\right]  \vec{r_{ij}}
+\end{aligned}
+```
+
+If ``\lambda`` is one this gives the standard [`LennardJones`](@ref) potential.
+"""
+@kwdef struct AshbaughHatch{C, H, S, E, L, W} <: PairwiseInteraction
+    cutoff::C = NoCutoff()
+    use_neighbors::Bool = false
+    shortcut::H = lj_zero_shortcut
+    σ_mixing::S = lorentz_σ_mixing
+    ϵ_mixing::E = lorentz_ϵ_mixing
+    λ_mixing::L = lorentz_λ_mixing
+    weight_special::W = 1
+end
+
+use_neighbors(inter::AshbaughHatch) = inter.use_neighbors
+
+function Base.zero(lj::AshbaughHatch{C, H, S, E, L, W}) where {C, H, S, E, L, W}
+    return AshbaughHatch(
+        lj.cutoff,
+        lj.use_neighbors,
+        lj.shortcut,
+        lj.σ_mixing,
+        lj.ϵ_mixing,
+        lj.λ_mixing,
+        zero(W),
+    )
+end
+
+function Base.:+(l1::AshbaughHatch, l2::AshbaughHatch)
+    return AshbaughHatch(
+        l1.cutoff,
+        l1.use_neighbors,
+        l1.shortcut,
+        l1.σ_mixing,
+        l1.ϵ_mixing,
+        l1.λ_mixing,
+        l1.weight_special + l2.weight_special,
+    )
+end
+
+@kwdef struct AshbaughHatchAtom{T, M, C, S, E, L}
+    index::Int = 1
+    atom_type::T = 1
+    mass::M = 1.0u"g/mol"
+    charge::C = 0.0
+    σ::S = 0.0u"nm"
+    ϵ::E = 0.0u"kJ * mol^-1"
+    λ::L = 1.0
+end
+
+@inline function force(inter::AshbaughHatch,
+                       dr,
+                       atom_i,
+                       atom_j,
+                       force_units=u"kJ * mol^-1 * nm^-1",
+                       special::Bool=false,
+                       args...)
+    if inter.shortcut(atom_i, atom_j)
+        return ustrip.(zero(dr)) * force_units
+    end
+
+    ϵ = inter.ϵ_mixing(atom_i, atom_j)
+    σ = inter.σ_mixing(atom_i, atom_j)
+    λ = inter.λ_mixing(atom_i, atom_j)
+
+    cutoff = inter.cutoff
+    r = norm(dr)
+    σ2 = σ^2
+    params = (σ2, ϵ, λ)
+
+    f = force_cutoff(cutoff, inter, r, params)
+    fdr = (f / r) * dr
+    if special
+        return fdr * inter.weight_special
+    else
+        return fdr
+    end
+end
+
+@inline function pairwise_force(::AshbaughHatch, r, (σ2, ϵ, λ))
+    r2 = r^2
+    six_term = (σ2 / r2) ^ 3
+    lj_term = (24ϵ / r) * (2 * six_term ^ 2 - six_term)
+    if r2 < (2^(1/3) * σ2)
+        return lj_term
+    else
+        return λ * lj_term
+    end
+end
+
+@inline function potential_energy(inter::AshbaughHatch,
+                                  dr,
+                                  atom_i,
+                                  atom_j,
+                                  energy_units=u"kJ * mol^-1",
+                                  special::Bool=false,
+                                  args...)
+    if inter.shortcut(atom_i, atom_j)
+        return ustrip(zero(dr[1])) * energy_units
+    end
+    ϵ = inter.ϵ_mixing(atom_i, atom_j)
+    σ = inter.σ_mixing(atom_i, atom_j)
+    λ = inter.λ_mixing(atom_i, atom_j)
+
+    cutoff = inter.cutoff
+    r = norm(dr)
+    σ2 = σ^2
+    params = (σ2, ϵ, λ)
+
+    pe = pe_cutoff(cutoff, inter, r, params)
+    if special
+        return pe * inter.weight_special
+    else
+        return pe
+    end
+end
+
+@inline function pairwise_pe(::AshbaughHatch, r, (σ2, ϵ, λ))
+    r2 = r^2
+    six_term = (σ2 / r2) ^ 3
+    lj_term = 4ϵ * (six_term ^ 2 - six_term)
+    if r2 < (2^(1/3) * σ2)
+        return lj_term + ϵ * (1 - λ)
+    else
+        return λ * lj_term
+    end
+end
